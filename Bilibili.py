bc1b32de68c5e11601175b3499b98d69df1035de
jingyuexing <1941755817@qq.com>
2020-04-19 22:41:34 +0800

    增加方法文档

--- Bilibili.py
+++ Bilibili.py
@@ -2,7 +2,7 @@
 # @Author: Moid
 # @Date:   2020-04-19 18:30:33
 # @Last Modified by:   jingyuexing
-# @Last Modified time: 2020-04-19 22:19:20
+# @Last Modified time: 2020-04-19 22:37:35
 
 import json
 import urllib3
@@ -12,7 +12,7 @@ http = urllib3.PoolManager()
 dataType = {
     "xml":"application/xml",
     "html":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "xhtml":"application/xhtml+xml"
+    "xhtml":"application/xhtml+xml",
     "json":"application/json, text/plain, */*",
     "text":"text/plain",
     "webp":"image/webp",
@@ -21,7 +21,7 @@ dataType = {
 head = {
   "Sec-Fetch-Mode":"no-cors",
   "Cache-Control":"max-age=0",
-  "Accept-Encoding":"gzip, deflate, br"
+  "Accept-Encoding":"gzip, deflate, br",
   "Accept-Language":"zh-CN,zh;q=0.9",
   "Accept":"application/json, text/plain, */*",
   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
@@ -30,15 +30,16 @@ with open("data/API.json", "r", encoding='utf-8') as file:
     api = json.loads(file.read())
     file.close()
 
+
 def requests(method='',url='',parma={}):
   '''发起请求
   
   [description]
   
   Keyword Arguments:
-    method {str} -- 方式(POST|GET|DELETE) (default: {''})
-    url {str} -- 链接 (default: {''})
-    parma {dict} -- 参数 (default: {{}})
+    method {str} -- 方式(POST|GET|DELETE) (default: {''})    
+    url {str} -- 链接 (default: {''})    
+    parma {dict} -- 参数 (default: {{}})    
   
   Returns:
     {dict} -- 返回数据
@@ -48,6 +49,19 @@ def requests(method='',url='',parma={}):
     return json.loads(req.data.decode("utf-8"),encoding='utf-8')
 
 def getRank(rankID=0,day=3,typer=1,arc_type=0):
+    '''获取排行榜
+    
+    [description]
+    
+    Keyword Arguments:
+        rankID {number} -- [description] (default: {0})
+        day {number} -- [description] (default: {3})
+        typer {number} -- [description] (default: {1})
+        arc_type {number} -- [description] (default: {0})
+    
+    Returns:
+        [type] -- [description]
+    '''
     config = api[0]
     method = config['method']
     url = config['link']
@@ -61,6 +75,16 @@ def getRank(rankID=0,day=3,typer=1,arc_type=0):
     return requests(method=method,url=url,fields=parma)
 
 def getUserInfor(userid=0):
+    '''获取用户信息
+    
+    [description]
+    
+    Keyword Arguments:
+        userid {number} -- [description] (default: {0})
+    
+    Returns:
+        [type] -- [description]
+    '''
     config:dict = api[2]
     method = config["method"]
     url = config["link"]
@@ -96,3 +120,5 @@ def getFanList(mid=0, pageNumber=1, limit=20):
     }
     return requests(method=method,url=url,fields=parma)
 
+if __name__ == '__main__':
+    print(api)
